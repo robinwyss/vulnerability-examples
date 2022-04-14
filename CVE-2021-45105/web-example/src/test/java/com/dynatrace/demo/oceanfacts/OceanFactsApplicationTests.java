ad54f462412011a4c32945ade1b6527326094810
Robin Wyss <me@robinwyss.com>
2022-04-14 11:57:09 +0200

    added web example for CVE-2021-45105

---
+++ CVE-2021-45105/web-example/src/test/java/com/dynatrace/demo/oceanfacts/OceanFactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynatrace.demo.oceanfacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanFactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
